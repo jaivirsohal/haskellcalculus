1622d786990805d3b8f38fcc4684249edab1cba2
Jaivir Sohal <js1424@ic.ac.uk>
2024-10-29 16:31:02 +0000

    added diff

--- src/Calculus.hs
+++ src/Calculus.hs
@@ -17,42 +17,67 @@ deriving instance Show Expr
 
 instance Num Expr where
   fromInteger = undefined
-  negate      = undefined
-  (+)         = undefined
-  (*)         = undefined
+  negate      = Neg
+  (+)         = Add
+  (*)         = Mul
 
 instance Fractional Expr where
   fromRational = undefined
-  (/)          = undefined
+  (/)          = Div
 
 instance Floating Expr where
-  sin = undefined
-  cos = undefined
-  log = undefined
+  sin = Sin
+  cos = Cos
+  log = Log
 
 ---------------------------------------------------------------------------
 
 lookUp :: Eq a => a -> [(a, b)] -> b
-lookUp = undefined
+lookUp key pair = fromJust (lookup key pair)
 
 {-|
 Evaluates a given expression, evaluating any variables to their value within
 the provided environment.
 -}
 eval :: Expr -> Env -> Double
-eval = undefined
+eval (Val x) _ = x
+eval (Id x) env = lookUp x env 
+eval (Add x y) env = eval x env + eval y env
+eval (Mul x y) env = eval x env * eval y env
+eval (Div x y) env = eval x env / eval y env
+eval (Neg x) env = -(eval x env)
+eval (Sin x) env = sin(eval x env)
+eval (Cos x) env = cos(eval x env)
+eval (Log x) env = log(eval x env)
 
 {-| OPTIONAL
 Pretty prints an expression to a more human-readable form.
 -}
 showExpr :: Expr -> String
-showExpr = undefined
+showExpr (Val x) = show x
+showExpr (Id x) = x
+showExpr (Add x y) = "(" ++ showExpr x ++ "+" ++ showExpr y ++ ")"
+showExpr (Mul x y) = "(" ++ showExpr x ++ "*" ++ showExpr y ++ ")"
+showExpr (Div x y) = "(" ++ showExpr x ++ "/" ++ showExpr y ++ ")"
+showExpr (Neg x) = "-(" ++ showExpr x ++ ")"
+showExpr (Sin x) = " (sin" ++ showExpr x ++ ")"
+showExpr (Cos x) = " (cos" ++ showExpr x ++ ")"
+showExpr (Log x) = " (log" ++ showExpr x ++ ")"
 
 {-|
 Symbolically differentiates a term with respect to a given identifier.
 -}
 diff :: Expr -> String -> Expr
-diff = undefined
+diff (Val a) x = Val 0
+diff (Id a) x =  if a == x then Val 1 else Val 0
+diff (Add a b) x = Add (diff a x) (diff b x)
+diff (Mul a b) x = Add (Mul a (diff b x)) (Mul (diff a x) b)
+diff (Div a b) x = Div (Add (Mul (diff a x) b) (Neg (Mul a (diff b x)))) (Mul b b)
+diff (Neg a) x = Neg (diff a x)
+diff (Sin a) x = Mul (Cos a) (diff a x)
+diff (Cos a) x = Neg (Mul (Sin a) (diff a x))
+diff (Log a) x = Div (diff a x) a
+
 
 {-|
 Computes the approximation of an expression `f` by expanding the Maclaurin
